866842a98172cc34f3b895fb8bec26f2b2a7c3e6
PhiHung21-0-00097 <0943244904hung@gmail.com>
2025-09-18 11:08:40 +0700

    up

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,47 @@
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { ValidationPipe } from '@nestjs/common';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   app.enableCors({
+//     origin: '*',
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     allowedHeaders: 'Content-Type, Accept',
+//   });
+//   app.useGlobalPipes(new ValidationPipe());
+//   await app.listen(3333);
+// }
+// bootstrap();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { Callback, Context, Handler } from 'aws-lambda';
+import serverlessExpress from '@vendia/serverless-express';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Accept',
-  });
-  app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3333);
+let server: Handler;
+
+async function bootstrap(): Promise<Handler> {
+  if (!server) {
+    const app = await NestFactory.create(AppModule);
+    app.enableCors({
+      origin: '*',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      allowedHeaders: 'Content-Type, Accept',
+    });
+    app.useGlobalPipes(new ValidationPipe());
+    await app.init();
+    const expressApp = app.getHttpAdapter().getInstance();
+    server = serverlessExpress({ app: expressApp });
+  }
+  return server;
 }
-bootstrap();
+
+export const handler: Handler = async (
+  event: any,
+  context: Context,
+  callback: Callback,
+) => {
+  const server = await bootstrap();
+  return server(event, context, callback);
+};

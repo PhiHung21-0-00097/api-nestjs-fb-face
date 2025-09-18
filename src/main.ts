eb0492ff204841313f9abd6fbe377b0442f2059b
PhiHung21-0-00097 <0943244904hung@gmail.com>
2025-09-18 11:10:51 +0700

    up

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,12 @@
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-// import { ValidationPipe } from '@nestjs/common';
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-//   app.enableCors({
-//     origin: '*',
-//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//     allowedHeaders: 'Content-Type, Accept',
-//   });
-//   app.useGlobalPipes(new ValidationPipe());
-//   await app.listen(3333);
-// }
-// bootstrap();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { Callback, Context, Handler } from 'aws-lambda';
 import serverlessExpress from '@vendia/serverless-express';
 
-let server: Handler;
+let cachedServer: any;
 
-async function bootstrap(): Promise<Handler> {
-  if (!server) {
+async function bootstrapServer() {
+  if (!cachedServer) {
     const app = await NestFactory.create(AppModule);
     app.enableCors({
       origin: '*',
@@ -32,16 +16,13 @@ async function bootstrap(): Promise<Handler> {
     app.useGlobalPipes(new ValidationPipe());
     await app.init();
     const expressApp = app.getHttpAdapter().getInstance();
-    server = serverlessExpress({ app: expressApp });
+    cachedServer = serverlessExpress({ app: expressApp });
   }
-  return server;
+  return cachedServer;
 }
 
-export const handler: Handler = async (
-  event: any,
-  context: Context,
-  callback: Callback,
-) => {
-  const server = await bootstrap();
-  return server(event, context, callback);
-};
+// ✅ Vercel yêu cầu default export
+export default async function handler(req: any, res: any) {
+  const server = await bootstrapServer();
+  return server(req, res);
+}

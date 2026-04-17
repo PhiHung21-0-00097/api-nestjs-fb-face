ab4641d5ae5354a2412e3c31489520b0646f14e6
Otis <0943244904hung@gmail.com>
2026-04-17 16:45:16 +0700

    up

--- src/main.ts
+++ src/main.ts
@@ -1,60 +1,60 @@
-// // deploy vercel
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-// import { ValidationPipe } from '@nestjs/common';
-
-// let appServer: any;
-
-// async function bootstrap() {
-//   if (!appServer) {
-//     const app = await NestFactory.create(AppModule);
-//     app.enableCors({
-//       origin: '*',
-//       methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-//       allowedHeaders: 'Content-Type, Accept, Authorization',
-//     });
-
-//     app.setGlobalPrefix('/api/v1');
-//     app.useGlobalPipes(new ValidationPipe());
-//     await app.init();
-//     appServer = app.getHttpAdapter().getInstance(); // Express instance
-//   }
-//   return appServer;
-// }
-
-// // ✅ Vercel yêu cầu default export
-// export default async function handler(req: any, res: any) {
-//   const appServer = await bootstrap();
-//   return appServer(req, res); // Trả về Express app
-// }
-
-// Local
-
+// deploy vercel
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+let appServer: any;
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  if (!appServer) {
+    const app = await NestFactory.create(AppModule);
+    app.enableCors({
+      origin: '*',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      allowedHeaders: 'Content-Type, Accept, Authorization',
+    });
 
-  app.enableCors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: 'Content-Type, Accept, Authorization',
-  });
-
-  app.setGlobalPrefix('/api/v1');
-  app.useGlobalPipes(new ValidationPipe());
-
-  await app.listen(3333);
-  const server = app.getHttpAdapter().getHttpServer();
-  const router = server._events.request._router; // Express router
-  console.log(
-    router.stack.map((r: any) => ({
-      path: r.route?.path,
-      methods: r.route?.methods,
-    })),
-  );
-  console.log(`🚀 Server is running on http://localhost:3333`);
+    app.setGlobalPrefix('/api/v1');
+    app.useGlobalPipes(new ValidationPipe());
+    await app.init();
+    appServer = app.getHttpAdapter().getInstance(); // Express instance
+  }
+  return appServer;
 }
-bootstrap();
+
+// ✅ Vercel yêu cầu default export
+export default async function handler(req: any, res: any) {
+  const appServer = await bootstrap();
+  return appServer(req, res); // Trả về Express app
+}
+
+// Local
+
+// import { NestFactory } from '@nestjs/core';
+// import { AppModule } from './app.module';
+// import { ValidationPipe } from '@nestjs/common';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+
+//   app.enableCors({
+//     origin: '*',
+//     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+//     allowedHeaders: 'Content-Type, Accept, Authorization',
+//   });
+
+//   app.setGlobalPrefix('/api/v1');
+//   app.useGlobalPipes(new ValidationPipe());
+
+//   await app.listen(3333);
+//   const server = app.getHttpAdapter().getHttpServer();
+//   const router = server._events.request._router; // Express router
+//   console.log(
+//     router.stack.map((r: any) => ({
+//       path: r.route?.path,
+//       methods: r.route?.methods,
+//     })),
+//   );
+//   console.log(`🚀 Server is running on http://localhost:3333`);
+// }
+// bootstrap();
